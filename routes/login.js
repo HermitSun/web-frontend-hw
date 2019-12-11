df548d2aaba8142f0c6838b49b05f5ec96063e07
Wen Sun <syl1887415157@126.com>
2019-12-11 11:21:45 +0800

    feat: 增加登录接口

--- routes/login.js
+++ routes/login.js
@@ -2,9 +2,41 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
+var modelUsers = require("../models/users");
+var StatusCode = require("../enums/status").StatusCodes;
+var ErrorMessages = require("../enums/status").ErrorMessages;
+
+// 查看页面
 router.get("/", function (req, res) {
   var loginPagePath = path.join(__dirname, "../public/login.html");
   res.sendFile(loginPagePath);
+});
+
+// 登录接口
+router.post("/", function (req, res) {
+  var status = StatusCode.OK;
+  var msg = ErrorMessages[status];
+
+  modelUsers.findByEmailAndPassword(
+    req.body.email,
+    req.body.password,
+    function (success, results) {
+      if (success) {
+        console.log(!results[0]);
+        if (!results[0]) {
+          status = StatusCode.NOT_FOUND;
+        }
+      } else {
+        status = StatusCode.INTERNAL_SERVER_ERROR;
+      }
+      msg = ErrorMessages[status];
+      // 在回调里返回
+      res.json({
+        status: status,
+        msg: msg
+      });
+    }
+  );
 });
 
 module.exports = router;
