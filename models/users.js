8de32037d63ef7b3070dae13e7d8be1de0e1c55d
Wen Sun <syl1887415157@126.com>
2019-12-11 11:22:55 +0800

    feat: 增加注册接口

--- models/users.js
+++ models/users.js
@@ -63,34 +63,34 @@ function getAllUsers(callback) {
  * 新增用户
  * @param email 用户邮箱
  * @param password 用户密码
- * @param callback 回调函数，第一个参数表示查询是否成功，第二个参数返回查询结果
+ * @param callback 回调函数，第一个参数表示查询是否成功
  */
 function addUser(email, password, callback) {
   var sql = "INSERT INTO users(email, password) VALUES (?, ?)";
-  users.query(sql, [email, password], function (err, results) {
+  users.query(sql, [email, password], function (err) {
     if (err) {
       console.log(err);
       callback(false);
       return;
     }
-    callback(true, results);
+    callback(true);
   });
 }
 
 /**
  * 删除用户
  * @param id 用户ID
- * @param callback 回调函数，第一个参数表示查询是否成功，第二个参数返回查询结果
+ * @param callback 回调函数，第一个参数表示查询是否成功
  */
 function removeUserById(id, callback) {
   var sql = "DELETE FROM users WHERE id = ?";
-  users.query(sql, [id], function (err, results) {
+  users.query(sql, [id], function (err) {
     if (err) {
       console.log(err);
       callback(false);
       return;
     }
-    callback(true, results);
+    callback(true);
   });
 }
 
--- routes/register.js
+++ routes/register.js
@@ -2,9 +2,36 @@ var express = require("express");
 var router = express.Router();
 var path = require("path");
 
+var modelUsers = require("../models/users");
+var StatusCode = require("../enums/status").StatusCodes;
+var ErrorMessages = require("../enums/status").ErrorMessages;
+
+// 查看页面
 router.get("/", function (req, res) {
   var registerPagePath = path.join(__dirname, "../public/register.html");
   res.sendFile(registerPagePath);
+});
+
+// 注册接口
+router.post("/", function (req, res) {
+  var status = StatusCode.OK;
+  var msg = ErrorMessages[status];
+
+  modelUsers.addUser(
+    req.body.email,
+    req.body.password,
+    function (success) {
+      if (!success) {
+        status = StatusCode.INTERNAL_SERVER_ERROR;
+      }
+      msg = ErrorMessages[status];
+      // 在回调里返回
+      res.json({
+        status: status,
+        msg: msg
+      });
+    }
+  );
 });
 
 module.exports = router;
